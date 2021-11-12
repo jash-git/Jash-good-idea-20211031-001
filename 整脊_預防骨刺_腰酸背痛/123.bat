7502ec7d7471760dbf56803d131af078a49033ef
jash.liao <jash.liao@gmail.com>
2021-11-12 11:06:51 +0800

    Add 202111121106

---
+++ 整脊_預防骨刺_腰酸背痛/123.bat
@@ -0,0 +1,4 @@
+ffmpeg -ss 00:00:10 -t 00:00:10 -i "videoplayback.mp4" -vcodec copy -acodec copy  "01.mp4"
+ffmpeg -ss 00:00:26 -t 00:00:10 -i "videoplayback.mp4" -vcodec copy -acodec copy  "02.mp4"
+ffmpeg -ss 00:00:40 -t 00:00:07 -i "videoplayback.mp4" -vcodec copy -acodec copy  "03.mp4"
+ffmpeg -ss 00:00:50 -t 00:00:37 -i "videoplayback.mp4" -vcodec copy -acodec copy  "04.mp4"
